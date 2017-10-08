9990aab983d2ae774efbf624c2b175fe08d757ec
Konstantin Nille-Hauf <konstantin.nillehauf@googlemail.com>
2017-10-09 00:23:31 +0200

    First Commit

---
+++ JANN/ViewModels/ShellViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JANN.ViewModels
+{
+    public class ShellViewModel : Screen 
+    {
+        private string _name = "test";
+
+        public string Name
+        {
+            get { return _name ; }
+            set {
+                _name = value;
+                NotifyOfPropertyChange(() => Name);
+            }
+        }
+
+    }
+}
